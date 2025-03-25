a91c003a3bfed01263b5c6b9e3b078d95152f979
Bogdan Kozlovskyi <kozlovskyi.bohdan@gmail.com>
2025-03-25 11:01:28 +0200

    finished lab 1

---
+++ lab1/cipher.py
@@ -0,0 +1,149 @@
+import random
+import numpy as np
+
+
+class SinglePermutationCipher:
+    def __init__(self):
+        pass
+
+    def encrypt(self, text):
+        key = list(range(1, len(text) + 1))
+        random.shuffle(key)
+
+        encrypted_text = [""] * len(text)
+        for original_index, new_index in enumerate(key, 1):
+            encrypted_text[new_index - 1] = text[original_index - 1]
+
+        encrypted_text = "".join(encrypted_text)
+        key = "-".join(str(i) for i in key)
+
+        return encrypted_text, key
+
+    def decrypt(self, encrypted_text, key):
+        decrypted_text = [""] * len(encrypted_text)
+        key = [int(i) for i in key.split("-")]
+        for new_index, original_index in enumerate(key):
+            decrypted_text[new_index] = encrypted_text[original_index - 1]
+
+        decrypted_text = "".join(decrypted_text)
+
+        return decrypted_text
+
+
+class RoutePermutationCipher:
+    def __init__(self):
+        pass
+
+    def encrypt(self, text):
+        text = text.replace(" ", "_")
+        blocks = []
+        for i in range(0, len(text), 4):
+            blocks.append(text[i : i + 4].ljust(4, "_"))
+        encrypted_figures = [[], [], []]
+        for block in blocks:
+            encrypted_figures[0].append(block[1])
+            encrypted_figures[1].append(block[0])
+            encrypted_figures[1].append(block[2])
+            encrypted_figures[2].append(block[3])
+
+        encrypted_text = "".join("".join(row) for row in encrypted_figures)
+
+        return encrypted_text
+
+    def decrypt(self, encrypted_text):
+        num_blocks = len(encrypted_text) // 4
+
+        decrypted_blocks = ["" for _ in range(num_blocks)]
+
+        index = 0
+        for i in range(num_blocks):
+            decrypted_blocks[i] = ["", "", "", ""]
+            decrypted_blocks[i][1] = encrypted_text[index]
+            index += 1
+
+        for i in range(num_blocks):
+            decrypted_blocks[i][0] = encrypted_text[index]
+            index += 1
+            decrypted_blocks[i][2] = encrypted_text[index]
+            index += 1
+
+        for i in range(num_blocks):
+            decrypted_blocks[i][3] = encrypted_text[index]
+            index += 1
+
+        decrypted_text = "".join("".join(block) for block in decrypted_blocks)
+        return decrypted_text.replace("_", " ").strip()
+
+
+class MultilevelCipher:
+    def __init__(self):
+        pass
+
+    def encrypt(self, text):
+        # Create blocks of 4 characters
+        blocks = []
+        for i in range(0, len(text), 4):
+            blocks.append(text[i : i + 4].ljust(4, "_"))
+
+        # Convert blocks to a matrix of characters
+        char_blocks = [[ch for ch in item] for item in blocks]
+        matrix = np.array(char_blocks)
+
+        # Get dimensions
+        row_length = len(matrix)
+        column_length = len(matrix[0])
+
+        # Generate random permutation indices
+        row_indices = np.random.permutation(row_length)
+        col_indices = np.random.permutation(column_length)
+
+        # Shuffle the matrix
+        shuffled_matrix = matrix[row_indices][:, col_indices]
+
+        # Flatten the shuffled matrix in column-major order
+        flattened_matrix = shuffled_matrix.flatten(order="F")
+
+        # Join into a single string
+        encrypted_text = "".join(flattened_matrix)
+
+        # Create encryption key from the permutation indices
+        encryption_key = (
+            ",".join(map(str, row_indices)) + "|" + ",".join(map(str, col_indices))
+        )
+
+        return encrypted_text, encryption_key
+
+    def decrypt(self, encrypted_text, encryption_key):
+        # Parse the encryption key to get row and column indices
+        row_indices_str, col_indices_str = encryption_key.split("|")
+        row_indices = np.array([int(x) for x in row_indices_str.split(",")])
+        col_indices = np.array([int(x) for x in col_indices_str.split(",")])
+
+        # Get dimensions from the key
+        row_length = len(row_indices)
+        column_length = len(col_indices)
+
+        # Create inverse permutation arrays for unshuffling
+        row_inverse = np.zeros(row_length, dtype=int)
+        for i, val in enumerate(row_indices):
+            row_inverse[val] = i
+
+        col_inverse = np.zeros(column_length, dtype=int)
+        for i, val in enumerate(col_indices):
+            col_inverse[val] = i
+
+        # Reshape the encrypted text to match the original shuffled matrix
+        encrypted_chars = list(encrypted_text)
+        reshuffled_matrix = np.array(encrypted_chars).reshape(
+            row_length, column_length, order="F"
+        )
+
+        # Unshuffle the matrix using the inverse permutation indices
+        unshuffled_matrix = reshuffled_matrix[row_inverse][:, col_inverse]
+
+        # Convert back to text and remove padding
+        decrypted_text = "".join(["".join(row) for row in unshuffled_matrix]).rstrip(
+            "_"
+        )
+
+        return decrypted_text
--- lab1/views.py
+++ lab1/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .cipher import SinglePermutationCipher, RoutePermutationCipher, MultilevelCipher
 
 
 # Create your views here.
@@ -7,4 +8,71 @@ def index(request):
 
 
 def lab1(request):
-    return render(request, "lab1.html")
+    context = {}
+
+    if request.method == "POST":
+        form_type = request.POST.get("form_type")
+        cipher = SinglePermutationCipher()
+        if form_type == "encrypt_form":
+            message = request.POST.get("message", "")
+            encrypted_message, encryption_key = cipher.encrypt(message)
+
+            # Add both original and processed messages to context
+            context["original_message"] = message
+            context["encrypted_message"] = encrypted_message
+            context["encryption_key"] = encryption_key
+
+            return render(request, "lab1.html", context)
+        elif form_type == "decrypt_form":
+            encrypted_message = request.POST.get("encrypted_message", "")
+            encryption_key = request.POST.get("encryption_key", "")
+            decrypted_message = cipher.decrypt(encrypted_message, encryption_key)
+
+            # Add both original and processed messages to context
+            context["decrypted_message"] = decrypted_message
+
+            return render(request, "lab1.html", context)
+        elif form_type == "route_encrypt_form":
+            message = request.POST.get("route_message", "")
+            encrypted_message = RoutePermutationCipher().encrypt(message)
+
+            # Add both original and processed messages to context
+            context["route_original_message"] = message
+            context["route_encrypted_message"] = encrypted_message
+
+            return render(request, "lab1.html", context)
+        elif form_type == "route_decrypt_form":
+            encrypted_message = request.POST.get("route_encrypted_message", "")
+            decrypted_message = RoutePermutationCipher().decrypt(encrypted_message)
+
+            # Add both original and processed messages to context
+            context["route_decrypted_message"] = decrypted_message
+
+            return render(request, "lab1.html", context)
+        elif form_type == "multilevel_encrypt_form":
+            message = request.POST.get("multilevel_message", "")
+            encrypted_message, encryption_key = MultilevelCipher().encrypt(message)
+
+            # Add both original and processed messages to context
+            context["multilevel_original_message"] = message
+            context["multilevel_encrypted_message"] = encrypted_message
+            context["multilevel_encryption_key"] = encryption_key
+
+            return render(request, "lab1.html", context)
+        elif form_type == "multilevel_decrypt_form":
+            encrypted_message = request.POST.get("multilevel_encrypted_message", "")
+            encryption_key = request.POST.get("multilevel_encryption_key", "")
+            decrypted_message = MultilevelCipher().decrypt(
+                encrypted_message, encryption_key
+            )
+
+            # Add both original and processed messages to context
+            context["multilevel_decrypted_message"] = decrypted_message
+
+            return render(request, "lab1.html", context)
+    else:
+        context["original_message"] = ""
+        context["encrypted_message"] = ""
+        context["encryption_key"] = ""
+
+        return render(request, "lab1.html", context)
